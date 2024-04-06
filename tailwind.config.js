dddf996be5008d9ff160c6cf38e2f3c1ef64ec60
Valentin <valentin.renaudin@my-digital-school.org>
2024-04-06 13:27:35 +0200

    [ADD] Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    colors: {
+      purple: '#5D47E9',
+    }, 
+    screens: {
+      'sm': {'max': '639px'}, // jusqu'à 640px
+      'md': {'max': '767px'}, // jusqu'à 768px
+      'lg': {'max': '1023px'}, // jusqu'à 1024px
+      'xl': {'max': '1279px'}, // jusqu'à 1280px
+      '2xl': {'max': '1535px'}, // jusqu'à 1536px
+    },
+
     extend: {},
   },
   plugins: [],
